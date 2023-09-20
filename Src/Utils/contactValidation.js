5770c4dd1423bf46062b1897d029b2beecab1bbc
FadeCap <stian94lb@gmail.com>
2023-09-20 21:05:31 +0200

    CSS finished for all pages / social links

--- Src/Utils/contactValidation.js
+++ Src/Utils/contactValidation.js
@@ -3,30 +3,44 @@ function validateForm() {
     var email = document.getElementById("email").value;
     var subject = document.getElementById("subject").value;
     var message = document.getElementById("message").value;
+
+    var nameError = document.getElementById("nameError");
+    var emailError = document.getElementById("emailError");
+    var subjectError = document.getElementById("subjectError");
+    var messageError = document.getElementById("messageError");
+
+    nameError.innerHTML = "";
+    emailError.innerHTML = "";
+    subjectError.innerHTML = "";
+    messageError.innerHTML = "";
+
     var errorMessages = "";
 
     if (name.length < 5) {
         errorMessages += "Name should be more than 5 characters long.<br>";
+        nameError.innerHTML = "Name should be more than 5 characters long.";
     }
 
     var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     if (!emailPattern.test(email)) {
         errorMessages += "Please enter a valid email address.<br>";
+        emailError.innerHTML = "Please enter a valid email address.";
     }
 
     if (subject.length < 15) {
         errorMessages += "Subject should be more than 15 characters long.<br>";
+        subjectError.innerHTML = "Subject should be more than 15 characters long.";
     }
 
     if (message.length < 25) {
         errorMessages += "Message content should be more than 25 characters long.<br>";
+        messageError.innerHTML = "Message content should be more than 25 characters long.";
     }
 
     if (errorMessages !== "") {
-        document.getElementById("errorMessages").innerHTML = errorMessages;
         return false;
     }
 
-
+    
     return true;
 }
