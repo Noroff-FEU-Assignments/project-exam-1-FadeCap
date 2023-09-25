5694bf7cf5f556037706c30290eaa3abec85750e
FadeCap <stian94lb@gmail.com>
2023-09-25 21:10:10 +0200

    Updated carousel.js to implement images
    Getting images from the /media endpoint

--- Src/Utils/carousel.js
+++ Src/Utils/carousel.js
@@ -10,15 +10,17 @@ const postsPerPage = 4;
 
 // Rendering blog posts into the carousel
 function createCarouselCard(post) {
+  // console.log(post)
   const card = document.createElement('div');
   card.classList.add('carousel-slide');
 
   const carouselImage = document.createElement('img');
-  carouselImage.src = post._links['wp:featuredmedia'][0].href
-  console.log(post._links['wp:featuredmedia'][0].href)
-
+  carouselImage.src = post.image;
+  
+  // console.log("Post image:", post.image)
   const carouselTitle = document.createElement('h2');
-  carouselTitle.textContent = post.title.rendered;
+  carouselTitle.textContent = post.title
+
 
 
   card.appendChild(carouselImage);
@@ -61,15 +63,28 @@ prevBtn.addEventListener('click', () => {
 
 // Event listener for the next button
 nextBtn.addEventListener('click', () => {
-  // You may need to fetch more posts if you run out
-  // Assuming you have a function fetchMorePosts() to fetch additional posts
-  // Use a similar approach to render them in the carousel
+
   currentSlide += postsPerPage;
   updateSlideVisibility();
 });
 
-// Initial setup: Fetch and render blog posts using the imported function
+
 getBlogs().then((posts) => {
+  // console.log("THIS ONE:", posts[0].image)
   renderBlogPosts(posts);
   updateSlideVisibility();
 });
+
+
+/* Posts[0]
+{
+  "id": 66,
+  "date": "2023-09-12T16:07:28",
+  "title": "Disc Golf Legends: Icons Who Shaped the Sport",
+  "content": "\n<p>Behind the growing popularity and evolution of disc golf, there are iconic figures who have left an indelible mark on the sport. These pioneers, champions, and innovators have shaped the course of disc golf&#8217;s history and development. In this post, we&#8217;ll pay tribute to some of the disc golf legends who have left a lasting legacy.</p>\n\n\n\n<p><strong>1. Ken Climo, &#8220;The Champ&#8221;:</strong> Ken Climo, with his unprecedented 12-time World Championship victories, is often regarded as one of the greatest disc golfers of all time. His consistency, putting prowess, and strategic gameplay have set a benchmark for generations to come.</p>\n\n\n\n<p><strong>2. Juliana Korver, &#8220;The Queen&#8221;:</strong> Juliana Korver is not only a multiple-time World Champion but also a passionate advocate for disc golf. Her dedication to growing the sport and her exemplary sportsmanship make her an iconic figure.</p>\n\n\n\n<p><strong>3. &#8220;Steady&#8221; Ed Headrick, Father of Disc Golf:</strong> Ed Headrick, the inventor of the modern Frisbee and the founder of the PDGA (Professional Disc Golf Association), laid the foundation for organized disc golf and the development of standardized discs and courses.</p>\n\n\n\n<p><strong>4. Dave Dunipace, Inventor of the Discraft Ultrastar:</strong> Dave Dunipace&#8217;s invention of the Discraft Ultrastar revolutionized disc sports. It became the standard for ultimate frisbee and significantly influenced the development of disc golf discs.</p>\n\n\n\n<p><strong>5. Avery Jenkins, Ambassador of the Sport:</strong> Avery Jenkins, a World Champion and disc golf ambassador, has played a vital role in promoting the sport globally, inspiring new players, and spreading the disc golf culture.</p>\n\n\n\n<p>These disc golf legends are just a few of the many individuals who have made significant contributions to the sport. Their achievements and dedication continue to inspire and shape the future of disc golf, ensuring that it remains a vibrant and evolving community of players and enthusiasts.</p>\n",
+  "mediaLink": "https://www.fadedisc.eu/wp-json/wp/v2/media/52",
+  "altText": "A group of disc golf players playing disc golf",
+  "image": "https://www.fadedisc.eu/wp-content/uploads/2023/09/group-play.jpg"
+}
+
+*/
--- Src/Utils/getBlogs.js
+++ Src/Utils/getBlogs.js
@@ -1,3 +1,5 @@
+import delay from "./delay.js";
+
 const apiConsumerKey = "ck_6397e4226cdc4c7e7f6b3c10c016a11b04152f39";
 const apiConsumerSecret = "cs_cec50018c62529280d1bc97683f3a4045c9d05ae";
 const apiUrl = "https://fadedisc.eu/wp-json/wp/v2/posts/";
@@ -17,8 +19,31 @@ const getBlogs = async () => {
       throw new Error(JSON.stringify({ status, statusText, url }));
     }
 
-    const blogs = await response.json();
-    return blogs;
+    const data = await response.json();
+    const blogData = data.map((fields) => ({
+      id: fields.id,
+      date: fields.date,
+      title: fields.title.rendered,
+      content: fields.content.rendered,
+      mediaLink: fields._links["wp:featuredmedia"][0].href,
+    }));
+
+    for (let i = 0; i < blogData.length; i++) {
+      // await delay(1000 * i);
+      const mediaUrl = await fetch(blogData[i].mediaLink, {
+        method: "GET",
+        headers: {
+          Authorization:
+            "Basic " + btoa(apiConsumerKey + ":" + apiConsumerSecret),
+        },
+      });
+      const data = await mediaUrl.json();
+
+      blogData[i].altText = await data.alt_text;
+      blogData[i].image = await data.source_url;
+    }
+
+    return await blogData;
   } catch (error) {
     console.error("Error occurred while trying to get blogs.\n", error);
   }
