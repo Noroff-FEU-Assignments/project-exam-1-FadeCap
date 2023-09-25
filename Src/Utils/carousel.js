9dc87516be9acdec2d379988e7407b36ab013043
FadeCap <stian94lb@gmail.com>
2023-09-26 00:05:00 +0200

    Carousel Complete :+1:

--- Src/Utils/carousel.js
+++ Src/Utils/carousel.js
@@ -1,16 +1,17 @@
 import getBlogs from './getBlogs.js';
 
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('#prev-btn');
+const nextBtn = document.querySelector('#next-btn');
 const carousel = document.querySelector('.carousel');
 
 // Set initial slide index and posts per page
 let currentSlide = 0;
 const postsPerPage = 4;
+let allPosts = [];
 
 // Rendering blog posts into the carousel
 function createCarouselCard(post) {
-  // console.log(post)
+
   const card = document.createElement('div');
   card.classList.add('carousel-slide');
 
@@ -18,7 +19,6 @@ function createCarouselCard(post) {
   carouselImage.classList.add('carousel-image');
   carouselImage.src = post.image;
   
-  console.log("Post image:", post.image)
   const carouselTitle = document.createElement('h2');
   carouselTitle.classList.add('carousel-title')
   carouselTitle.textContent = post.title
@@ -33,6 +33,7 @@ function createCarouselCard(post) {
 }
 
 function renderBlogPosts(posts) {
+  allPosts = posts
   const cards = posts
     .slice(currentSlide, currentSlide + postsPerPage)
     .map((post) => createCarouselCard(post));
@@ -57,17 +58,27 @@ function updateSlideVisibility() {
 
 // Event listener for the previous button
 prevBtn.addEventListener('click', () => {
+  console.log(currentSlide)
   if (currentSlide > 0) {
     currentSlide -= postsPerPage;
-    updateSlideVisibility();
+    renderBlogPosts(allPosts);
+  } else {
+    currentSlide = 8
+    renderBlogPosts(allPosts)
   }
 });
 
 // Event listener for the next button
 nextBtn.addEventListener('click', () => {
+  console.log("72.6", currentSlide)
+  if (currentSlide + postsPerPage < allPosts.length) {
 
-  currentSlide += postsPerPage;
-  updateSlideVisibility();
+    currentSlide += postsPerPage;
+    renderBlogPosts(allPosts);
+  } else {
+    currentSlide = 0
+    renderBlogPosts(allPosts)
+  }
 });
 
 
@@ -75,17 +86,3 @@ getBlogs().then((posts) => {
   renderBlogPosts(posts);
   updateSlideVisibility();
 });
-
-
-/* Posts[0]
-{
-  "id": 66,
-  "date": "2023-09-12T16:07:28",
-  "title": "Disc Golf Legends: Icons Who Shaped the Sport",
-  "content": "\n<p>Behind the growing popularity and evolution of disc golf, there are iconic figures who have left an indelible mark on the sport. These pioneers, champions, and innovators have shaped the course of disc golf&#8217;s history and development. In this post, we&#8217;ll pay tribute to some of the disc golf legends who have left a lasting legacy.</p>\n\n\n\n<p><strong>1. Ken Climo, &#8220;The Champ&#8221;:</strong> Ken Climo, with his unprecedented 12-time World Championship victories, is often regarded as one of the greatest disc golfers of all time. His consistency, putting prowess, and strategic gameplay have set a benchmark for generations to come.</p>\n\n\n\n<p><strong>2. Juliana Korver, &#8220;The Queen&#8221;:</strong> Juliana Korver is not only a multiple-time World Champion but also a passionate advocate for disc golf. Her dedication to growing the sport and her exemplary sportsmanship make her an iconic figure.</p>\n\n\n\n<p><strong>3. &#8220;Steady&#8221; Ed Headrick, Father of Disc Golf:</strong> Ed Headrick, the inventor of the modern Frisbee and the founder of the PDGA (Professional Disc Golf Association), laid the foundation for organized disc golf and the development of standardized discs and courses.</p>\n\n\n\n<p><strong>4. Dave Dunipace, Inventor of the Discraft Ultrastar:</strong> Dave Dunipace&#8217;s invention of the Discraft Ultrastar revolutionized disc sports. It became the standard for ultimate frisbee and significantly influenced the development of disc golf discs.</p>\n\n\n\n<p><strong>5. Avery Jenkins, Ambassador of the Sport:</strong> Avery Jenkins, a World Champion and disc golf ambassador, has played a vital role in promoting the sport globally, inspiring new players, and spreading the disc golf culture.</p>\n\n\n\n<p>These disc golf legends are just a few of the many individuals who have made significant contributions to the sport. Their achievements and dedication continue to inspire and shape the future of disc golf, ensuring that it remains a vibrant and evolving community of players and enthusiasts.</p>\n",
-  "mediaLink": "https://www.fadedisc.eu/wp-json/wp/v2/media/52",
-  "altText": "A group of disc golf players playing disc golf",
-  "image": "https://www.fadedisc.eu/wp-content/uploads/2023/09/group-play.jpg"
-}
-
-*/
--- Src/Utils/getBlogs.js
+++ Src/Utils/getBlogs.js
@@ -2,7 +2,7 @@ import delay from "./delay.js";
 
 const apiConsumerKey = "ck_6397e4226cdc4c7e7f6b3c10c016a11b04152f39";
 const apiConsumerSecret = "cs_cec50018c62529280d1bc97683f3a4045c9d05ae";
-const apiUrl = "https://fadedisc.eu/wp-json/wp/v2/posts/";
+const apiUrl = "https://fadedisc.eu/wp-json/wp/v2/posts/?per_page=20";
 
 const getBlogs = async () => {
   try {
@@ -12,8 +12,7 @@ const getBlogs = async () => {
         Authorization:
           "Basic " + btoa(apiConsumerKey + ":" + apiConsumerSecret),
       },
-    });
-
+    })
     if (!response.ok) {
       const { status, statusText, url } = response;
       throw new Error(JSON.stringify({ status, statusText, url }));
