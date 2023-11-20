32706bae46a4da139a1286cfdec3164c232eef8a
FadeCap <stian94lb@gmail.com>
2023-11-21 00:46:20 +0100

    getblogs.js updated loading speed

--- Src/Utils/getBlogs.js
+++ Src/Utils/getBlogs.js
@@ -1,9 +1,10 @@
 const apiConsumerKey = "ck_6397e4226cdc4c7e7f6b3c10c016a11b04152f39";
 const apiConsumerSecret = "cs_cec50018c62529280d1bc97683f3a4045c9d05ae";
-const apiUrl = "https://fadedisc.eu/wp-json/wp/v2/posts/?per_page=12";
+const apiUrl = "https://fadedisc.eu/wp-json/wp/v2/posts/?per_page=12&_embed=wp:featuredmedia";
 
 const getBlogs = async () => {
   try {
+    console.time('Posts')
     const response = await fetch(apiUrl, {
       method: "GET",
       headers: {
@@ -17,6 +18,7 @@ const getBlogs = async () => {
     }
 
     const data = await response.json();
+    console.log(data)
     const blogData = data.map((fields) => ({
       id: fields.id,
       date: fields.date,
@@ -24,21 +26,11 @@ const getBlogs = async () => {
       content: fields.content.rendered,
       mediaLink: fields._links["wp:featuredmedia"][0].href,
       description: fields.excerpt.rendered,
+      altText: fields._embedded['wp:featuredmedia'][0].alt_text,
+      image: fields._embedded['wp:featuredmedia'][0].source_url
     }));
-
-    for (let i = 0; i < blogData.length; i++) {
-      const mediaUrl = await fetch(blogData[i].mediaLink, {
-        method: "GET",
-        headers: {
-          Authorization:
-            "Basic " + btoa(apiConsumerKey + ":" + apiConsumerSecret),
-        },
-      });
-      const data = await mediaUrl.json();
-
-      blogData[i].altText = await data.alt_text;
-      blogData[i].image = await data.source_url;
-    }
+    console.log(blogData)
+    console.timeEnd('Posts')
     return await blogData;
   } catch (error) {
     console.error("Error occurred while trying to get blogs.\n", error);
